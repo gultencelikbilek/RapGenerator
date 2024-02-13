a7640f645279ffa5a423735e428a12af44cf197b
gultencelikbilek <81778970+gultencelikbilek@users.noreply.github.com>
2024-02-13 18:58:49 +0300

    GeneratingLyrcisFragment,ApiService,DependencyInjection,Retrofit added this structure

--- app/src/main/java/com/example/rapgenerator/utils/Constants.kt
+++ app/src/main/java/com/example/rapgenerator/utils/Constants.kt
@@ -2,5 +2,6 @@ package com.example.rapgenerator.utils
 
 object Constants {
     const val BASE_URL = "https://api.openai.com/v1/"
+    const val END_POINT = "chat/completions"
     const val CHAT_API_KEY = "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"
 }
